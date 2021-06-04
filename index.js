2c59c275f04bcebf722d4f9683eb5247d0dc501a
Đoàn Thế Hào <haodt1990@gmail.com>
2021-06-04 10:45:18 +0700

    add crypto endpoint

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const CryptoJS = require("crypto-js");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/api/encrypt", async (req, res) => {
+  const encrypted = CryptoJS.AES.encrypt(
+    req.body.message,
+    req.body.secret
+  ).toString();
+
+  res.send({
+    data: encrypted,
+  });
+});
+
+app.post("/api/decrypt", async (req, res) => {
+  const decrypted = CryptoJS.AES.decrypt(
+    req.body.message,
+    req.body.secret
+  ).toString(CryptoJS.enc.Utf8);
+
+  res.send({
+    data: decrypted,
+  });
+});
+
+app.listen(process.env.PORT || 8080);
